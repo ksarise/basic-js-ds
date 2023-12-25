9d3d110fb9fa5395ddb86af2d8ef82562abd024d
Ksarise <sergkrav.social@gmail.com>
2023-12-25 16:16:57 +0700

    init: start task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -10,32 +10,26 @@ class BinarySearchTree {
 
   root() {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   add(/* data */) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   has(/* data */) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   find(/* data */) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   remove(/* data */) {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   min() {
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 
   max() {
