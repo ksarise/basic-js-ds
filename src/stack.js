59141f2161a882abc6d2f74604a54a11856b1148
Ksarise <sergkrav.social@gmail.com>
2023-12-25 22:18:18 +0700

    feat: finish stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.stack = [];
+    this.size = 0;
+  }
+  push(element) {
+    // this.stack.push(element);
+    this.stack[this.size] = element;
+    this.size += 1;
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    // this.stack.pop();
+    delete this.stack[this.size];
+    this.size -= 1;
+    return this.stack[this.size];
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.size-1];
   }
 }
 
