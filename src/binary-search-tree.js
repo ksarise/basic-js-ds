0cf958ffeeb16abf537c3b93647d0b46a71bdf4b
Ksarise <sergkrav.social@gmail.com>
2023-12-26 01:09:16 +0700

    feat:add binary tree has & remove

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -33,29 +33,59 @@ class BinarySearchTree {
     }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  has(data) {
+    return this.searchNode(this.rootTree, data) !== null;
   }
 
   find(data) {
-    return searchNode(this.rootTree, data);
-    function searchNode(node, data) {
-      if (!node) {
-        return null;
-      }
-      if (node.data === data) {
-        return node;
-      }
-      if (data > node.data) {
-        return searchNode(node.right, data);
-      } else {
-        return searchNode(node.left, data);
-      }
+    return this.searchNode(this.rootTree, data);
+  }
+
+  searchNode(node, data) {
+    if (!node) {
+      return null;
+    }
+    if (node.data === data) {
+      return node;
+    }
+    if (data > node.data) {
+      return this.searchNode(node.right, data);
+    } else {
+      return this.searchNode(node.left, data);
     }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  remove(data) {
+    this.rootTree = removeNode(this.rootTree, data)
+    function removeNode(node, value) {
+      if (!node) {
+        return null;
+      }
+      if (value < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if (value > node.data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+        if (!node.left) {
+          return node.right;
+        }
+        if (!node.right) {
+          return node.left;
+        }
+        let rightMin = node.right;
+        while (rightMin.left) {
+          rightMin = rightMin.left;
+        }
+        node.data = rightMin.data;
+        node.right = removeNode(node.right, rightMin.data);
+        return node;
+      }
+    }
   }
 
   min() {
